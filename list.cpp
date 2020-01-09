021953330a862488347ef05fdeab0189e053a6a6
0xA1F <azb.0x7dc@gmail.com>
2020-01-09 11:41:51 +0200

    3rd day of Mr. Smartass - move to templates

--- list.cpp
+++ list.cpp
@@ -3,32 +3,34 @@
 #include <memory>
 
 
+template<typename T>
 class LinkedList {
     private:
-        uint64_t _value;
+        T _value;
         uint64_t _length;
         LinkedList* next;
 
     public:
         LinkedList() : _value{}, _length{0}, next{nullptr} {}
 
-        LinkedList(uint64_t value) : _value{value}, _length{1}, next{nullptr} {};
+        LinkedList(T value) : _value{value}, _length{1}, next{nullptr} {};
 
         ~LinkedList() {
+            std::cout << "Called destructor for value: " << std::to_string(this->_value) << " \n"; 
             delete next;
         }
 
         auto length() const -> uint64_t { return _length; }
 
-        auto getValue() const -> uint64_t { return _value; }
+        auto value() const -> T { return _value; }
 
-        void append(const uint64_t value) {
+        void append(const T value) {
             if (length() == 0) {
                 _value = value;
                 _length = 1;
             }
             else {
-                auto newNode = new LinkedList(value);
+                auto newNode = new LinkedList<T>(value);
                 auto lastNode = last();
                 lastNode->next = newNode;
                 _length += 1;
@@ -36,8 +38,8 @@ class LinkedList {
 
         }
 
-        LinkedList* prepend(const uint64_t value) {
-            auto newNode = new LinkedList(value);
+        LinkedList* prepend(const T value) {
+            auto newNode = new LinkedList<T>(value);
             newNode->next = this;
             newNode->_length = _length + 1;
             return newNode;
@@ -71,13 +73,13 @@ class LinkedList {
 
 
 int main() {
-    auto ll = new LinkedList();
+    auto ll = new LinkedList<uint64_t>();
     ll->append(25);
     ll->append(35);
     ll->append(45);
     ll->append(84);
     ll = ll->prepend(144);
-    std::cout << ll->last()->getValue() << "\n";
+    std::cout << ll->last()->value() << "\n";
     std::cout << ll->toString() << "\n";
     std::cout << ll->length() << "\n";
     delete ll;
